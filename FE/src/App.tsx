7c5420a2efc05b3956c31ab8e3755ca11b4b1ac1
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2025-06-05 04:13:25 +0700

    manage service

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -10,6 +10,7 @@ import RegisterPage from "./pages/Register";
 import AdminDashboard from "./pages/admin/Dashboard";
 import AccountList from "./pages/admin/AccountList";
 import ConsultantDetailPage from "./pages/ConsultantDetailPage";
+import Service from "./pages/admin/Service";
 
 function App() {
   return (
@@ -22,7 +23,7 @@ function App() {
         <Route path="/admin/blogs" element={<MainLayout><div className="p-6 bg-white rounded-lg shadow-sm">Quản lý blog</div></MainLayout>} />
         <Route path="/admin/consulting" element={<MainLayout><div className="p-6 bg-white rounded-lg shadow-sm">Quản lý tư vấn</div></MainLayout>} />
         <Route path="/admin/reports" element={<MainLayout><div className="p-6 bg-white rounded-lg shadow-sm">Báo cáo thống kê</div></MainLayout>} />
-        <Route path="/admin/settings" element={<MainLayout><div className="p-6 bg-white rounded-lg shadow-sm">Cài đặt hệ thống</div></MainLayout>} />
+        <Route path="/admin/services" element={<MainLayout><Service /></MainLayout>} />
         
         {/* Routes cho User */}
         <Route path="/" element={<Home />} />
