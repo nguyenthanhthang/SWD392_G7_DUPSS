ed5ef11847fa5a0148d49fdb1a52ec5999dce62e
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2025-06-10 02:24:42 +0700

    change icon working schedule

--- FE/src/pages/admin/Consultant.tsx
+++ FE/src/pages/admin/Consultant.tsx
@@ -768,16 +768,7 @@ const Consultant: React.FC = () => {
                       </button>
                     </Tooltip>
 
-                    <Tooltip text="Lịch làm việc">
-                      <button
-                        onClick={() => handleOpenScheduleModal(consultant)}
-                        className="p-2 rounded-full bg-green-100 text-green-600 hover:bg-green-200 transition-colors"
-                      >
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                        </svg>
-                      </button>
-                    </Tooltip>
+
                   </div>
                 </td>
               </tr>
