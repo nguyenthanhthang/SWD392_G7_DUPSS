5748900f78e2f432610c4659faf31ad8ef368556
Nguyễn Thanh Thắng <nguyenthanhthang190500@gmail.com>
2025-07-08 00:53:12 +0700

    fix js to ts

--- BE/src/routes/blogRoutes.ts
+++ BE/src/routes/blogRoutes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 import {
   getAllBlogs,
   getBlogById,
@@ -8,28 +8,28 @@ import {
   getBlogsByUserId,
   addComment,
   deleteComment,
-  getComments
-} from '../controllers/blogController';
-const upload = require('../middleware/uploadImage');
+  getComments,
+} from "../controllers/blogController";
+import upload from "../middleware/uploadImage";
 
 const router = express.Router();
 
 // Lấy tất cả blogs
-router.get('/', getAllBlogs);
+router.get("/", getAllBlogs);
 // Lấy blog theo id
-router.get('/:id', getBlogById);
+router.get("/:id", getBlogById);
 // Tạo blog mới (có upload ảnh)
-router.post('/', upload.single('image'), createBlog);
+router.post("/", upload.single("image"), createBlog);
 // Cập nhật blog (có upload ảnh)
-router.put('/:id', upload.single('image'), updateBlog);
+router.put("/:id", upload.single("image"), updateBlog);
 // Xóa blog
-router.delete('/:id', deleteBlog);
+router.delete("/:id", deleteBlog);
 // Lấy blog theo userId
-router.get('/user/:userId', getBlogsByUserId);
+router.get("/user/:userId", getBlogsByUserId);
 
 // Comment routes
-router.get('/:id/comments', getComments);
-router.post('/:id/comments', addComment);
-router.delete('/:blogId/comments/:commentId', deleteComment);
+router.get("/:id/comments", getComments);
+router.post("/:id/comments", addComment);
+router.delete("/:blogId/comments/:commentId", deleteComment);
 
-export default router; 
+export default router;
