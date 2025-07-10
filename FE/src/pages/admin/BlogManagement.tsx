cd416338d065eec5d6f6e3fd2371a097cb228a05
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2025-07-11 00:12:17 +0700

    fix admin change status member blog

--- FE/src/pages/admin/BlogManagement.tsx
+++ FE/src/pages/admin/BlogManagement.tsx
@@ -604,7 +604,7 @@ const BlogManagement: React.FC = () => {
                             <option value="rejected">Từ chối</option>
                           </select>
                         </div>
-                      ) : isAdminBlog(blog) ? (
+                      ) : (blog.published === 'published' || blog.published === 'unpublished') ? (
                         <div className="flex items-center gap-1">
                           <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400 mr-1 group-hover:text-sky-500 transition-colors" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
