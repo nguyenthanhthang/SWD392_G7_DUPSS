4da30cd9992e011c35bbdcfe90b682ef3a35a50c
Nguyễn Thanh Thắng <nguyenthanhthang190500@gmail.com>
2025-07-26 08:36:40 +0700

    fix cros

--- BE/src/index.ts
+++ BE/src/index.ts
@@ -37,7 +37,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "*", // Cho phép tất cả domain tạm thời
+    origin: [
+      "https://swd-392-g7-dupss-hcbq-dei7blx2l.vercel.app",
+      "https://g7dupss.com",
+      "http://localhost:3000",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
